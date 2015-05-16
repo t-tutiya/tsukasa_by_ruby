e5dedaa6962cbf8d1e4df3f2ada4debb4eac9177
RPGP1 <great.super.science@gmail.com>
2015-05-16 21:00:51 +0900

    define / call_functionコマンド
    procedure / alias関連を削除・defineメソッドとして統一
    動作はRubyのdefに近くした
    使用例はscenario_functionを参照

--- system/script_compiler.rb
+++ system/script_compiler.rb
@@ -40,7 +40,7 @@ class ScriptCompiler
     @key_name = :commands
     @key_name_stack = []
 
-    @alias_list = []
+    @function_list = []
 
     if block
       self.instance_exec(argument, &block)
@@ -126,18 +126,8 @@ class ScriptCompiler
   end
 
   #プロシージャー登録されたコマンドが宣言された場合にここで受ける
-  def method_missing(command_name, target: nil, **options, &block)
-    #メソッド名が識別子リストに登録されていない場合
-    #親クラスに伝搬し、syntax errorとする
-    return super if !@alias_list.include?(command_name)
-
-    #call_aliasコマンドとして登録
-    #TODO:一時的にprocedureの機能を停止（aliasと機能を使い分ける方法を再考）
-    #TODO:現状ブロックのみでoptionsは対応していない。optionも受け取らない（最終的には全部反映したい）
-    options[:__alias_name] = command_name
-    impl(:call_alias, :Anonymous, target, nil, options)do
-      if block; @key_name = :commands; block.call; end
-    end
+  def method_missing(command_name, target: nil, **options)
+    impl(:call_function, :Anonymous, target, command_name, options)
   end
 
   #次フレームに送る
@@ -248,21 +238,11 @@ class ScriptCompiler
     impl(:block, :Anonymous, target, nil, &block)
   end
 
-=begin
-    #TODO：一時的にprocedureの機能を停止する
-    #プロシージャー宣言
-    #TODOプロシージャーリストへの追加処理を足す
-    def procedure(command_name,target: nil , **sub)
-      impl(:procedure, :LayoutContainer,target, command_name, sub)
-      @alias_list.push(command_name)
-    end
-=end
-  #コマンド群に別名を設定する
-  def ALIAS(command_name, target: nil, &block)
-    impl(:alias, :LayoutContainer, target, command_name)do
-      if block; @key_name = :commands; block.call; end
-    end
-    @alias_list.push(command_name)
+  #TODO:製作者「仕様変更も歓迎です」
+  #target変更は受け付けない(定義した時に扱っているコントロールに登録)
+  #自分の子コントロール内ならaboutすればいい
+  def define(command_name, &block)
+    impl(:define, :Anonymous, nil, command_name, {block: block})
   end
 
   #制御構造関連

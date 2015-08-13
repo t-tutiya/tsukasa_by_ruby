3a20201440af4af57ef4225b475b81d541f3de73
t_tutiya <skip081-nowornever@yahoo.co.jp>
2015-08-13 15:30:48 +0900

    コリジョンのサンプルを追加

--- scenario/scenario_collision.rb
+++ scenario/scenario_collision.rb
@@ -30,8 +30,64 @@ _CREATE_ :ButtonControl,
     set :key_down, visible: true
   end
   on_key_up do
-    set :key_down, visible: false
     set :normal, visible: false
     set :over,   visible: true
+=begin
+    #TODO:幾つか足りない処理があったのでひとまずコメントアウト
+    _SEND_ :key_down do
+      transition_fade frame: 15,
+        count: 0,
+        start: 255,
+        last: 0
+    end
+    _WAIT_ [:command, :skip], command: :transition_fade
+=end
+    set :key_down, visible: false
+  end
+end
+
+
+_CREATE_ :ButtonControl, 
+        :x_pos => 450, 
+        :y_pos => 150, 
+        :width => 256,
+        :height => 256,
+        :id=>:button2,
+        :collision=>[128,0,0,255,255,255] do
+  image :entity=>Image.new(256,256).triangle_fill(128,0,0,255,255,255,C_BLUE).draw_font(80, 120, "NORMAL", Font.default),
+        :id=>:normal
+  image :entity=>Image.new(256,256).triangle_fill(128,0,0,255,255,255,C_YELLOW).draw_font(80, 120, "OVER", Font.default, C_BLACK),
+        :id=>:over, :visible => false
+  image :entity=>Image.new(256,256).triangle_fill(128,0,0,255,255,255,C_GREEN).draw_font(80, 120, "DOWN", Font.default),
+        :id=>:key_down, :visible => false
+  on_mouse_over do
+    set :normal, visible: false
+    set :over,   visible: true
+    set :key_down, visible: false
+  end
+  on_mouse_out do
+    set :over,   visible: false
+    set :normal, visible: true
+    set :key_down, visible: false
+  end
+  on_key_down do
+    set :over,   visible: false
+    set :normal, visible: false
+    set :key_down, visible: true
+  end
+  on_key_up do
+    set :normal, visible: false
+    set :over,   visible: true
+=begin
+    #TODO:幾つか足りない処理があったのでひとまずコメントアウト
+    _SEND_ :key_down do
+      transition_fade frame: 15,
+        count: 0,
+        start: 255,
+        last: 0
+    end
+    _WAIT_ [:command, :skip], command: :transition_fade
+=end
+    set :key_down, visible: false
   end
 end

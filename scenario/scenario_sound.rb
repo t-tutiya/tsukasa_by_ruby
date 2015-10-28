2a9bc8a8d45b28dbb3708c1cbb23e1a6b7777389
t_tutiya <skip081-nowornever@yahoo.co.jp>
2015-10-28 17:17:48 +0900

    sound関連のサンプルコードを更新

--- scenario/scenario_sound.rb
+++ scenario/scenario_sound.rb
@@ -28,74 +28,47 @@
 #[The zlib/libpng License http://opensource.org/licenses/Zlib]
 ###############################################################################
 
-text "スペースキーを押すとDirectSoundの初期化します"
-line_feed
-text "多少時間がかかります。"
-page_pause
+#動作しない。tksからコンバートが必要
+raise
 
-_CREATE_ :SoundControl, file_path: "./test/bell.wav", id: :test
+キーを押してしばらくお待ちください。
 
-text "初期化が終了しました。スペースキーを押すと再生します"
+@	_CREATE_ :SoundControl, file_path: "./test/bell.wav", id: :test01
+初期化が終了しました。キーを押すと再生します
 
-page_pause
+@	test01{_SET_ play: true}
 
-_SEND_ :test do
-  play
-end
+再生中。スペースキーを押すと停止します。
 
-text "再生中。スペースキーを押すと停止します。"
+@	test01{_SET_ play: false}
 
-page_pause
+停止。次は無限ループ
 
-_SEND_ :test do
-  stop
-end
+@	test01{
+@		_SET_ loop_count: -1
+@		_SET_ play: true
+@	}
 
-text "停止。次は無限ループ"
+無限ループ中。スペースキーを押すと停止します。
 
-page_pause
+@	test01{_SET_ play: false}
 
-_SEND_ :test do
-  _SET_ loop_count: -1
-  play
-end
+停止。次はフェードイン
 
-text "無限ループ中。スペースキーを押すと停止します。"
+@	test01{
+@		_MOVE_ 180, volume: [0,230]
+@		_SET_ play: true
+@	}
 
-page_pause
+フェードインしつつ無限ループ中
+スペースキーを押すとフェードアウトします
 
-_SEND_ :test do
-  stop
-end
+@	test01{
+@		_SET_ play: false
+@		_MOVE_ 180, volume: [230, 0]
+@		_SET_ play: true
+@	}
 
-text "停止。次はフェードイン"
+フェードアウトしつつ無限ループ中。スペースキーを押すと停止します
 
-page_pause
-
-_SEND_ :test do
-  fade start: 0, last: 230, fade_ms: 960
-  play
-end
-
-text "フェードインしつつ無限ループ中"
-line_feed
-text "スペースキーを押すとフェードアウトします"
-line_feed
-text "上手く動いてませんが原因不明"
-
-page_pause
-
-_SEND_ :test do
-  #初期値は省略可
-  fade last: 0, fade_ms: 960
-  play
-end
-
-text "フェードアウトしつつ無限ループ中。スペースキーを押すと停止します"
-
-page_pause
-
-_SEND_ :test do
-  stop
-end
-
+@	test01{_SET_ play: false}

8df1822b24fd95ab37e60af64e0e1babcd4ced92
t_tutiya <skip081-nowornever@yahoo.co.jp>
2015-12-04 18:49:10 +0900

    Soundコントロールのインターフェイスを更新
    Ayameのインターフェイスを全部外に出した

--- system/sound_control.rb
+++ system/sound_control.rb
@@ -1,7 +1,7 @@
 #! ruby -E utf-8
 
 require 'dxruby'
-#require 'ayame'
+require 'ayame'
 
 ###############################################################################
 #TSUKASA for DXRuby  α１
@@ -34,79 +34,26 @@ require 'dxruby'
 
 #Soundコントロール
 class SoundControl  < Control
-
-  #リピート回数
-  attr_reader :loop_count
-  def loop_count=(loop_count)
-    @loop_count = loop_count
-    @entity.loop_count = [0, loop_count].max
-  end
-
-#音量
-  attr_reader :volume
-  def volume=(volume)
-    @volume = volume
-    @entity.set_volume(@volume, 0)
-  end
-
-  #パン
-  attr_reader :pan
-  def pan=(args)
-    @pan = args
-    @entity.set_pan(@pan, 0)
-  end
-
-  #再生／停止
-  attr_reader :play
-  def play=(args)
-    @play = args
-    if @play
-      @entity.play(@loop_count)
-    else
-      @entity.stop
-    end
-  end
-
-  attr_reader :file_path
-  def file_path=(file_path)
-    @file_path = file_path
-    #ayameでコントロールを初期化
-    @entity = Ayame.new(@file_path)
-    #ストリーム駆動であれば処理を終える
-    return if stream?
-    #音源ファイルを先読みする
-    if File.extname(@file_path) == ".ogg"
-      @entity.predecode
-    else
-      @entity.prefetch
-    end
-  end
-
-  attr_accessor :stream
-  def stream?
-    @stream
-  end
-
   def initialize(options, inner_options, root_control)
     super
-    self.stream = options[:stream] || true
-    self.file_path = options[:file_path]
+    if options[:file_path]
+      options[:_ARGUMENT_] = options[:file_path]
+      command__LOAD_(options, inner_options)
+    end
+  end
 
-    #ループ回数（－１なら無限ループ）
-    self.loop_count = options[:loop_count] || 1
-
-    #ボリューム／フェード指定
-    self.volume = options[:volume] || 90
+  def update
+    Ayame.update
+    super
   end
 
   def siriarize(options = {})
-
     options.update({
       :file_path => @file_path,
       :start => @start,
       :loop_start => @loop_start,
       :loop_end => @loop_end,
-      :loop_count => @loop_count,
+      #:loop_count => @loop_count,
       :volume => @volume,
     })
 
@@ -120,5 +67,45 @@ class SoundControl  < Control
       @entity = nil
     end
     super
+  end
+  
+  #ファイルの読み込み
+  def command__LOAD_(options, inner_options)
+    raise unless options[:_ARGUMENT_]
+    #ayameでコントロールを初期化
+    @entity = Ayame.new(options[:_ARGUMENT_])
+  end
+
+  #再生
+  def command__PLAY_(options, inner_options)
+    @entity.play( options[:_ARGUMENT_] || 1, #ループ回数（０なら無限）
+                  options[:fadetime].to_f || 0)
+  end
+
+  #停止
+  def command__STOP_(options, inner_options)
+    @entity.stop(options[:fadetime].to_f || 0)
+  end
+
+  #一時停止
+  def command__PAUSE_(options, inner_options)
+    @entity.pause(options[:fadetime].to_f || 0)
+  end
+
+  #再開
+  def command__RESUME_(options, inner_options)
+    @entity.resume(options[:fadetime].to_f || 0)
+  end
+
+  #音量
+  def command__VOLUME_(options, inner_options)
+    @entity.set_volume( options[:_ARGUMENT_] || 90,
+                        options[:fadetime].to_f || 0)
+  end
+
+  #パン
+  def command__PAN_(options, inner_options)
+    @entity.set_pan( options[:_ARGUMENT_] || 0, #-100～100
+                     options[:fadetime].to_f || 0)
   end
 end

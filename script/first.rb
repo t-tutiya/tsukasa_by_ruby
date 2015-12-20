8ae208533a0319b9f6f63a798022ea5dc04a96a0
t_tutiya <skip081-nowornever@yahoo.co.jp>
2015-12-20 10:47:33 +0900

    サンプルコードのINCLUDEを別ファイルに移した

--- script/first.rb
+++ script/first.rb
@@ -1,33 +1,5 @@
-
-#_INCLUDE_ "./script/sample/sample_1_1.tks" 	#文字の表示
-#_INCLUDE_ "./script/sample/sample_1_2.tks" 	#文字装飾
-
-#_INCLUDE_ "./script/sample/sample_1_3.tks" 	#コントロールの表示
-#_INCLUDE_ "./script/sample/sample_1_4.tks" 	#移動
-#_INCLUDE_ "./script/sample/sample_1_5.tks" 	#線形補間
-
-#_INCLUDE_ "./script/sample/sample_1_6.tks" 	#フェードイン・アウト
-#_INCLUDE_ "./script/sample/sample_1_7.tks" 	#レイアウト
-
-#_INCLUDE_ "./script/sample/sample_1_8.tks" 	#クロスフェード
-#_INCLUDE_ "./script/sample/sample_1_9.tks" 	#トランジション
-
-#_INCLUDE_ "./script/sample/sample_1_10.tks"	#ルールトランジション
-
-#_INCLUDE_ "./script/sample/sample_1_11.tks"	#ユーザー定義コマンド
-#_INCLUDE_ "./script/sample/sample_1_12.tks"	#データ領域／インラインデータ
-
-#_INCLUDE_ "./script/sample/sample_1_13.tks"	#条件分岐
-#_INCLUDE_ "./script/sample/sample_1_14.tks"	#ループ
-
-#_INCLUDE_ "./script/sample/sample_1_15.tks"	#ボタン
-#_INCLUDE_ "./script/sample/sample_1_16.tks"	#サウンド
-#_INCLUDE_ "./script/sample/sample_1_17.tks"	#セーブロード
-
-#ボタンサンプル
-#_INCLUDE_ "./script/sample/sample_2_1_1.rb" 	#ボタンサンプル１
-#_INCLUDE_ "./script/sample/sample_2_1_2.rb" 	#ボタンサンプル２
-#_INCLUDE_ "./script/sample/sample_2_1_3.rb" 	#ボタンサンプル３
+#サンプルコード
+#_INCLUDE_ "./script/sample/sample.rb"
 
 #デモゲーム
-_INCLUDE_ "./script/demo_game/1_0.tks" 	#ゲームサンプル
+_INCLUDE_ "./script/demo_game/1_0.tks"

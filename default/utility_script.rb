87d5ff5cec71cd45a37266fd1032e31d5d84149f
t_tutiya <skip081-nowornever@yahoo.co.jp>
2016-04-02 14:15:03 +0900

    リファクタリング
    サンプルコード更新

--- default/utility_script.rb
+++ default/utility_script.rb
@@ -97,9 +97,13 @@ _DEFINE_ :_END_PAUSE_ do
           _ADD_TILE_GROUP_ file_path: "./resource/icon/icon_4_a.png",
             x_count: 4, y_count: 1
           _STACK_LOOP_ do
+            _MAP_STATUS_ 0
             _WAIT_ count: 5
+            _MAP_STATUS_ 1
             _WAIT_ count: 5
+            _MAP_STATUS_ 2
             _WAIT_ count: 5
+            _MAP_STATUS_ 3
             _WAIT_ count: 5
           end
         end

59b57e35656dc9a6281ee3b90cdeb6eb60aa3fe4
t_tutiya <skip081-nowornever@yahoo.co.jp>
2016-12-04 15:28:48 +0900

    テストコード更新

--- test/test.rb
+++ test/test.rb
@@ -1,5 +1,5 @@
 #! ruby -E utf-8
-
+require 'pp'
 require 'minitest/test'
 require '../system/Tsukasa.rb'
 
@@ -9,34 +9,23 @@ MiniTest.autorun
 
 class TC_Foo < Minitest::Test
   def test_foo
-    tsukasa = Tsukasa::Window.new({ :width => 640,
-                                    :height => 480,
-                                    })
+    tsukasa = Tsukasa::Control.new() do
+      _PUTS_ "test_foo"
+      _EXIT_
+    end
 
     DXRuby::Window.loop() do
-      #ここでテストを行う想定
-      #司エンジン処理
       tsukasa.update(DXRuby::Input.mouse_x, DXRuby::Input.mouse_y, 0)
       tsukasa.render(0, 0, DXRuby::Window)
-
-      #スクリプトで終了コマンドが実行された場合
-      break if tsukasa.close?
+      break if tsukasa.exit
     end
-  end
-
-  def test_bar
-    tsukasa = Tsukasa::Window.new({ :width => 640,
-                                    :height => 480,
-                                    })
-
-    DXRuby::Window.loop() do
-      #ここでテストを行う想定
-      #司エンジン処理
-      tsukasa.update(DXRuby::Input.mouse_x, DXRuby::Input.mouse_y, 0)
-      tsukasa.render(0, 0, DXRuby::Window)
-
-      #スクリプトで終了コマンドが実行された場合
-      break if tsukasa.close?
-    end
+    
+    reslut = [[:_SET_,
+                {:id=>:"Tsukasa::Control",
+                 :child_update=>true,
+                 :script_parser=>{},
+                 :exit=>true},
+                {}]]
+    assert_equal(tsukasa.serialize(), reslut, "NO")
   end
 end

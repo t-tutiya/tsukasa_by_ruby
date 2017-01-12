45684bcf92a8dae9a1a4919563cc06c09a0452e5
t_tutiya <skip081-nowornever@yahoo.co.jp>
2017-01-12 10:18:47 +0900

    サンプル追加

--- test/test_textpage_base.rb
+++ test/test_textpage_base.rb
@@ -40,7 +40,7 @@ MiniTest.autorun
 
 class TestTextPageBase < Minitest::Test
 
-  def test_2017_1_1_1_テキスト表示テスト
+  def test_2017_1_11_1_テキスト表示テスト
     #コントロールの生成
     control = Tsukasa::TextPage.new({}, nil,nil,nil) do
       _TEXT_ "テキストウィンドウテスト表示１"
@@ -70,7 +70,7 @@ class TestTextPageBase < Minitest::Test
     end
   end
 
-  def test_2017_1_1_2_レンダリング済みフォント表示テスト
+  def test_2017_1_11_2_レンダリング済みフォント表示テスト
     #コントロールの生成
     control = Tsukasa::Window.new({}, nil,nil,nil) do
       #デフォルトのユーザー定義コマンド群の読み込み
@@ -97,6 +97,72 @@ class TestTextPageBase < Minitest::Test
       _LOOP_ 2 do
         _END_FRAME_
       end
+      #メインループを終了する
+      _EXIT_
+    end
+
+    #メインループ
+    DXRuby::Window.loop() do
+      control.update(DXRuby::Input.mouse_x, DXRuby::Input.mouse_y, 0) #処理
+      control.render(0, 0, DXRuby::Window) #描画
+      break if control.exit #メインループ終了判定
+    end
+  end
+
+  def test_2017_1_12_1_標準テキストウィンドウ表示テスト
+    #コントロールの生成
+    control = Tsukasa::Window.new() do
+      #ヘルパーコントロール群
+
+      #データストアコントロール
+      require './system/Data.rb'
+      #タイルマップ管理
+      require './system/TileMap.rb'
+      #シェーダー処理
+      require './system/Shader.rb'
+      #ルールトランジション
+      require './system/RuleTransition.rb'
+
+      #デフォルトのユーザー定義コマンド群の読み込み
+      _INCLUDE_ "./default/default_script.rb"
+
+      #標準ユーティリティー群の読み込み
+      _INCLUDE_ "./default/utility_script.rb"
+
+      #タイトルバーの文字列を設定
+      _SET_ caption: "Tsukasa Engine powered by DXRuby"
+
+      #一時データストア
+      _CREATE_ :Data, id: :_TEMP_
+
+      #キー入力管理コントロール
+      _CREATE_ :Input, id: :_INPUT_
+
+      #文字描画
+      require './system/Char.rb'
+      #テキストページ管理
+      require './system/TextPage.rb'
+      #TKSパーサーと関連するテキストレイヤのセットアップ
+      _INCLUDE_ "./default/text_layer_script.rb"
+      #初期テキストウィンドウ
+      _TEXT_WINDOW_ :text0, 
+        x: 96,
+        y: 256,
+        width: 1024,
+        height: 192,
+        size: 32, 
+        font_name: "ＭＳＰ ゴシック",
+        z: 1000000 #描画順序
+      #初期テキストウィンドウのidを格納
+      _DEFINE_PROPERTY_ _DEFAULT_TEXT_PAGE_: [:_ROOT_, :text0]
+
+      _SEND_ [:_ROOT_, :text0] do
+        _FLUSH_
+        _RUBI_ "□□□□□□□"
+        _TEXT_ "■■■■■■■"
+      end
+      _END_PAUSE_
+
       #メインループを終了する
       _EXIT_
     end
